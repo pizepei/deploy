71127c9043018bf729ec1daed01b4ce7754acb75
皮泽培 <848733913@qq.com>
2019-06-19 10:07:42 +0800

    初始化创建

---
+++ src/Ssh2.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pizepei
+ * Date: 2019/6/19
+ * Time: 10:06
+ */
+namespace pizepei\deploy;
+class Ssh2
+{
+    /**
+     * Ssh2 constructor.初始化
+     */
+    public function __construct()
+    {
+
+
+    }
+
+
+
+
+}
