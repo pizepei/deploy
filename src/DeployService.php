1616d32f6f92dfe94b78c194407f298324a4757c
pizepei <848733913@qq.com>
2019-06-25 22:30:40 +0800

    完善部署操作

--- src/DeployService.php
+++ src/DeployService.php
@@ -9,6 +9,9 @@
 
 namespace pizepei\deploy;
 
+use function MongoDB\BSON\fromJSON;
+use pizepei\deploy\model\DeployServerConfigModel;
+use pizepei\deploy\model\DeployServerRelevanceModel;
 use pizepei\deploy\model\GitlabMicroServiceDeployConfigModel;
 use pizepei\deploy\model\GitlabSystemHooksModel;
 use pizepei\staging\Controller;
@@ -76,25 +79,63 @@ class DeployService
          * 判断当前操作用户是否有权限
          * trigger_user
          */
-
-        var_dump($MicroServiceData['trigger_user']);
-
-        $data['object_kind'] = $data['object_kind'];
-        $data['service_name'] = $data['project']['name'];
-        $data['service_description'] = $data['project']['description'];
-        $data['trigger_user'] = [
-            $data['user_id']=>[
-                'user_name'=>$data['user_name'],
-                'user_email'=>$data['user_email'],
-                'user_avatar'=>$data['user_avatar'],
-            ]
+        if(!isset($MicroServiceData['trigger_user'][$data['user_id']]))
+        {
+            return ['error'=>'当前操作用户网权限'];
+        }
+        /**
+         * 获取微服务服务器部署配置
+         */
+        $ServerRelevance = DeployServerRelevanceModel::table();
+        $where = [
+            'micro_service'=>$MicroServiceData['id'],
         ];
+        $ServerRelevanceData = $ServerRelevance->where($where)->fetch();
+        /**
+         * 获取目标服务器配置
+         */
+        $ServerConfig = DeployServerConfigModel::table()->get($ServerRelevanceData['serve_id']??'');
+
+        /**
+         * 创建链接ssh2
+         */
+        $config=[
+            'host'=>$ServerConfig['server_ip'],
+            'port'=>$ServerConfig['ssh2_port'],
+            'username'=>$ServerConfig['ssh2_user'],
+            'password'=>$ServerConfig['ssh2_password'],
+            'ssh2_auth'=>$ServerConfig['ssh2_auth'],//pubkey  or password
+            'pubkey'=>$ServerConfig['ssh2_pubkey'],//这里的公钥对不是必须为当前用户的
+            'prikey'=>$ServerConfig['ssh2_prikey'],//
+        ];
+        $SSH = new Ssh2($config);
+        /**
+         * 批量准备shll脚本
+         */
+
+        /**
+         * 循环执行
+         */
+
+        //var_dump($ServerConfig);
+        //var_dump($MicroServiceData['trigger_user']);
+
+        //$data['object_kind'] = $data['object_kind'];
+        //$data['service_name'] = $data['project']['name'];
+        //$data['service_description'] = $data['project']['description'];
+        //$data['trigger_user'] = [
+        //    $data['user_id']=>[
+        //        'user_name'=>$data['user_name'],
+        //        'user_email'=>$data['user_email'],
+        //        'user_avatar'=>$data['user_avatar'],
+        //    ]
+        //];
 //        $res = $MicroService->add($data);
 
-        if (!empty($res)){
-            $res = reset($res);
-            var_dump($res);
-        }
+        //if (!empty($res)){
+        //    $res = reset($res);
+        //    //var_dump($res);
+        //}
     }
     /**
      * @Author 皮泽培
